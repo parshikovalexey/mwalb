a3a0705307d02500d38c2cdeb20e06ad10926c6a
Roman Dremlyugov <dremlygov@gmail.com>
2019-01-25 12:37:56 +0700

    Подготовка к релизу (#21)
    Добавлен новый UI интерфейс

---
+++ ProcessDocument.WPF/GostViewer.xaml.cs
@@ -0,0 +1,63 @@
+﻿using StandardsLibrary;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Controls;
+using MahApps.Metro.Controls;
+using StandardsLibrary.Simple;
+
+namespace ProcessDocument.WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для GostViewer.xaml
+    /// </summary>
+    public partial class GostViewer : MetroWindow, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+
+
+        GostModel _currentGostModel = new GostModel();
+        private List<GostModelViewer> _view;
+
+        public List<GostModelViewer> View
+        {
+            get => _view;
+            set { _view = value; NotifyPropertyChanged("View"); }
+        }
+
+        public GostViewer(GostModel gostModel)
+        {
+            InitializeComponent();
+            DataContext = this;
+            _currentGostModel = gostModel;
+            View = GetModelForView();
+            NotifyPropertyChanged("View");
+        }
+
+        private List<GostModelViewer> GetModelForView()
+        {
+            var result = new List<GostModelViewer>();
+
+            result.Add(new GostModelViewer(){ Header="Стиль основного текста", Model=_currentGostModel.GlobalText});
+            result.Add(new GostModelViewer(){ Header= "Стиль изображений", Model=_currentGostModel.Headline});
+            result.Add(new GostModelViewer(){ Header= "Стиль заголовков", Model=_currentGostModel.Image});
+
+
+
+            return result;
+        }
+    }
+
+    public class GostModelViewer
+    {
+        public string Header { get; set; }
+        public SimpleStyle Model { get; set; }
+    }
+}
